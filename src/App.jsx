61ab2905c62e6ca7c88d9d81f3038704992ff038
An0nym0usShad0w <76418474+rafiaksd@users.noreply.github.com>
2025-11-09 11:11:05 +0600

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <>
       <div className='font-primary'>
           <Navbar />
+          <Hero />
       </div> 
     </>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import bannerImg from "../assets/banner.png"
+
+const Hero = () => {
+  return (
+     <section className='bg-white pt-12' id='about'>
+          <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 md:py-24'>
+               <div className='flex flex-col md:flex-row items-center gap-6'>
+                    {/* left side */}
+                    <div className='md:w-1/2'>
+                         <div className='md:w-4/5'>
+                              <h1 className='text-4xl font-bold text-secondary mb-4'>Navigating the digital world for success</h1>
+                              <p className='text-md md:text-lg text-gray-600 mb-6'>Our digital marketing agency helps businesses grow and succeed online, through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+                              <button className='inline-block px-6 py-3 bg-black text-white font-semibold rounded-md hover:bg-primary hover:text-black transition-all duration-150'>Book a consultation</button>
+                         </div>
+                    </div>
+
+                    {/* right side */}
+                    <div className='md:w-1/2 order-first md:order-last'>
+                         <img src={bannerImg} alt="banner-img" className='w-full h-auto rounded-md'></img>
+                    </div>
+               </div>
+          </div>
+
+          {/* brand logo */}
+          
+     </section>
+  )
+}
+
+export default Hero
